2c4f3ec43b1a69a1aa1751de33b01ac2b333d48d
yavuz <yavuz.celik.05@gmail.com>
2021-01-10 00:15:57 +0300

    font cause sarya hoca

---
+++ Assets/textTyper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textTyper : MonoBehaviour
+{
+    public string TEXT;
+    public int framePerBeat,LetterPerBeat;
+    private int currentframe = 0, currentBeat = 0;
+    private TMPro.TextMeshProUGUI textMesh;
+    // Update is called once per frame
+    private void Start()
+    {
+        textMesh = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+    void Update()
+    {
+        try
+        {
+
+     
+        currentframe++;
+        if (currentframe==framePerBeat)
+        {
+            currentframe = 0;
+            currentBeat++;
+
+
+            textMesh.SetText( TEXT.Remove(currentBeat * LetterPerBeat).Replace(@"ü", @"<br>"));
+            if (TEXT.Remove(currentBeat * LetterPerBeat)==TEXT)
+            {
+                this.enabled = false;
+            }
+        }
+        }
+        catch (System.Exception)
+        {
+
+            this.enabled = false;
+        }
+    }
+}
