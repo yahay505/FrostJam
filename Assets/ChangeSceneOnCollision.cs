3eb5d71a6565413e05a69eec2aaa176406994b62
yavuz <yavuz.celik.05@gmail.com>
2021-01-10 00:24:44 +0300

    cumartesi kapanış sceneloading works yarım herşey bok olmazsa iyi olur

---
+++ Assets/ChangeSceneOnCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnCollision : MonoBehaviour
+{
+    public int NextScene;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(NextScene, LoadSceneMode.Single);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(NextScene, LoadSceneMode.Single);
+
+    }
+}
