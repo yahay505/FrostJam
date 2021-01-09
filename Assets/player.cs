c5d90805d1093cbd3794ef22759f7d1ca3edaa70
yavuz <yavuz.celik.05@gmail.com>
2021-01-09 13:51:43 +0300

    Initial commit

---
+++ Assets/player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public SpriteRenderer colorrenderer;
+    public Color[] colors;
+    private Vector3 mousePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProccessCollorWheel();
+    }
+
+    private void ProccessCollorWheel()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Time.timeScale = .2f;
+            mousePos = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            Time.timeScale = 1;
+            Vector2 v2 =  new Vector2( Input.mousePosition.x, Input.mousePosition.y) - new Vector2(mousePos.x,mousePos.y);
+
+ 
+            float angle = Mathf.Atan2(v2.y, v2.x) * Mathf.Rad2Deg;
+            Debug.Log(angle);
+        }
+    }
+}
