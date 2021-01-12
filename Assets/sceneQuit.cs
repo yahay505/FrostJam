b3ef804098acd25af76772eaa724411ffdd05baa
SaranBaran <69807636+SaranBaran@users.noreply.github.com>
2021-01-12 22:02:14 +0300

    son

---
+++ Assets/sceneQuit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneQuit : MonoBehaviour
+{
+
+        public void quitGame() {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+}
+
